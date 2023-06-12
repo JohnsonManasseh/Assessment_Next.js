2673ca54df8548c52719dc9ab591e0f00594a3cc
Johnson manasseh <johnsonrockstar@gmail.com>
2023-06-13 01:31:22 +0530

    hover changes and responsive modal

--- components/Card.tsx
+++ components/Card.tsx
@@ -24,25 +24,12 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ image }) => {
   const { urls, user, likes } = image;
 
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleMouseEnter = () => {
-    if (image.user) {
-      setIsHovered(true);
-    }
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
-
   return (
-    <div   className={styles['card']} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-         {isHovered && (
+    <div   className={styles['card']} >
         <div className={styles['card-details']}>
          <LongParagraph text={user.bio} likes={likes} maxLength={40} user={user} />
         </div>
-      )}
+     
       <Image className={styles['responsive-image']} src={urls.regular} alt={user.instagram_username} loading="lazy" width={270} height={300} />
       <div className={styles['card-info']}>
       </div>
