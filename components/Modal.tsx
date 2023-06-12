a433a77628a3795c4ec19dd4fbc939a6fdad2850
Johnson manasseh <johnsonrockstar@gmail.com>
2023-06-12 17:37:49 +0530

    modal changes

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,5 +1,4 @@
 import styles from '../app/page.module.css';
-import Image from 'next/image';
 
 interface ModalProps {
   text: string;
@@ -16,8 +15,6 @@ const Modal: React.FC<ModalProps> = ({ text, closeModal, likes, user }) => {
   return (
     <div className={styles['modal-container']}>
       <div className={styles['modal-content']}>
-      <Image className={styles['profile-image']} src={user.profile_image.large} alt={user.profile_image.large} loading="lazy" width={128} height={128} />
-      <p><strong>Likes - {likes}</strong></p>
         <p>{text}</p>
         <button className={styles['modal-close']} onClick={closeModal}>
           Close
